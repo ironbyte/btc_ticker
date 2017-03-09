ec706d8194acd1c9c5545be4972f12918745614c
ironbyte <illusivespectre@gmail.com>
2017-03-09 15:43:27 +0200

    Configure WebMock to disable net connect (Localhost only)

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,6 +3,8 @@ require "btc_ticker"
 
 require 'webmock/rspec'
 
+WebMock.disable_net_connect!(allow_localhost: true)
+
 RSpec.configure do |config|
   # Enable flags like --only-failures and --next-failure
   config.example_status_persistence_file_path = ".rspec_status"
