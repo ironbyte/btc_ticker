fa5aba9d583653aa0d2e2d7847281c99053fa47e
ironbyte <illusivespectre@gmail.com>
2017-04-03 19:28:52 +0200

    Refactor and clean up the code in the spec file

--- spec/btc_ticker_spec.rb
+++ spec/btc_ticker_spec.rb
@@ -1,30 +1,28 @@
 require "spec_helper"
 
 describe "BtcTicker::Bitstamp::API::Ticker" do
-  context "Given the input parameter - btc_usd" do
-    describe "#get_json" do
-      let(:ticker) { BtcTicker::Bitstamp::API::Ticker.new({:pair => "btc_usd"}) }
 
+  describe "#get_json" do
+    let(:ticker_btcusd) { BtcTicker::Bitstamp::API::Ticker.new({:pair => "btc_usd"}) }
+    let(:ticker_btceur) { BtcTicker::Bitstamp::API::Ticker.new({:pair => "btc_eur"}) }
+
+    context "Given the input parameter - btc_usd" do
       it "must return the parsed btc_usd json data as an Hash object" do
-        expect(ticker.get_json).to be_an_instance_of Hash
+        expect(ticker_btcusd.get_json).to be_an_instance_of Hash
       end
 
       it "must not be empty" do
-        expect(ticker.get_json).to_not be_nil
+        expect(ticker_btcusd.get_json).to_not be_nil
       end
     end
-  end
 
-  context "Given the input parameter - btc_eur" do
-    describe "#get_json" do
-      let(:ticker) { BtcTicker::Bitstamp::API::Ticker.new({:pair => "btc_eur"}) }
-
+    context "Given the input parameter - btc_eur" do
       it "must return the parsed btc_eur json data as an Hash object" do
-        expect(ticker.get_json).to be_an_instance_of Hash
+        expect(ticker_btceur.get_json).to be_an_instance_of Hash
       end
 
       it "must not be empty" do
-        expect(ticker.get_json).to_not be_nil
+        expect(ticker_btceur.get_json).to_not be_nil
       end
     end
   end
